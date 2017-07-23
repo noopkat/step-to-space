9adaaee429878f83a0dd8f7b3d56f2714548d131
Suz Hinton <suz.hinton@gmail.com>
2017-07-23 12:57:48 -0400

    added basic parcel data and display logic

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,88 @@
   const spaceStationDistance = 640; // doubled for one 'lap'
   const pixelSpaceStationDistance = 700;
 
+  interface parcel {
+    label: string;
+    imageFilename: string;
+  }
+
+  interface IDatedParcelList {
+    [propName: string]: parcel;
+  }
+
+  const miscParcelList: parcel[] = [
+     {
+       label: "beef jerky",
+       imageFilename: "jerky.png"
+     },
+     {
+       label: "space icecream",
+       imageFilename: "icecream.png"
+     },
+     {
+       label: "kitten",
+       imageFilename: "kitten.png"
+     },
+     {
+       label: "space sushi",
+       imageFilename: "sushi.png"
+     },
+     {
+       label: "toilet paper",
+       imageFilename: "toiletpaper.png"
+     }
+  ];
+  
+  // change this to a nested structure ie. [month][day]
+  const datedParcelList: IDatedParcelList = {
+    '23-0': {
+      label: "Jack D. Fischer's birthday cake",
+      imageFilename: "cake.png"
+    },
+    '3-0': {
+      label: "Commander Fyodor Yurchikhin's birthday cake",
+      imageFilename: "cake.png"
+    },
+    '9-1': {
+      label: "Dr. Peggy A. Whitson's birthday cake",
+      imageFilename: "cake.png"
+    }
+  };
+
+/*
+  space icecrem
+  kittens in space
+  telescope
+  space sushi
+  beer
+  coffee
+  kimchi
+  tea
+  fruit rollups
+  toilet paper
+  shrubbery
+  laptop
+  tim tams
+  sonic scredriver
+  3d filament
+  pizza
+  a towel 25th may
+  space pen
+  fidget spinner
+  tang
+  batteries
+  arduinos
+  solarpanels
+  sunglasses
+  milo*/
+
   interface astrodata {
     distance: number;
     numlaps: number;
     position: number;
     progress: number;
     classname: string;
-  };
+  }
 
   const calculateAstronaut = function(distance: number) {
     return new Promise((resolve, reject) => {
@@ -37,11 +112,7 @@
   }
 
   const updateAstronaut = function(data: astrodata) {
-    const {distance, numlaps, position, progress, classname} = data;
-
-    // update sentence UI
-    dist.textContent = `${distance}km`;
-    laps.textContent = numlaps.toString();
+    const {position, progress, classname} = data;
 
     // update astronaut UI
     astronaut.className = classname;
@@ -57,6 +128,28 @@
     heart.style.animation = `pounding ${aLength}s linear infinite alternate`;
   };
 
+  const updateSentence = function(data: astrodata) {
+    const {distance, numlaps} = data;
+    
+    // update sentence UI
+    dist.textContent = `${distance}km`;
+    laps.textContent = numlaps.toString();
+  };
+
+  const updateParcel = function() {
+    const today = new Date();
+    const prop = `${today.getDate()}-${today.getMonth()}`;
+    let parcel;
+
+    if (datedParcelList[prop]) {
+      parcel = datedParcelList[prop];
+    } else {
+      const index = Math.floor(Math.random() * miscParcelList.length); 
+      console.log('index:', index);
+      parcel = miscParcelList[index];
+    }
+    parcelContainer.textContent = parcel.label;
+  };
 
   // for debugging
   // let i = 0;
@@ -68,7 +161,11 @@
   fetch('/api/distance')
     .then((response: any) => response.json())
     .then(({ distance }) => calculateAstronaut(250))
-    .then((data: astrodata) => updateAstronaut(data)); 
+    .then((data: astrodata) => {
+      updateAstronaut(data);
+      updateSentence(data);
+      updateParcel();
+    }); 
 
   fetch('/api/heart')
     .then((response: any) => response.json())
